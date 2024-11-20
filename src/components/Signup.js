5fd990c0b32f7523d2e021454d7fe556d4895be2
Shashwat Seth <67456677+sethshashwat@users.noreply.github.com>
2024-11-20 23:00:12 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -11,7 +11,7 @@ const Signup = (props) => {
 
     const onSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:4000/api/auth/createuser", {
+        const response = await fetch("https://inotebook-backend-8csl.onrender.com/api/auth/createuser", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
