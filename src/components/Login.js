b09d832c716215ecc7bd0f3f99304e82de101662
Shashwat Seth <67456677+sethshashwat@users.noreply.github.com>
2024-11-20 22:59:53 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,7 +11,7 @@ const Login = (props) => {
 
     const onSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:4000/api/auth/login", {
+        const response = await fetch("https://inotebook-backend-8csl.onrender.com/api/auth/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
